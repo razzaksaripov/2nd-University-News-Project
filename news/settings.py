bb02e1c6883e80cc260f60a090a73eb56116197f
Razzak <[200~razzaksaripov@gmail.com>
2023-12-20 20:16:28 +0600

    all i need it a looooove to night

--- news/settings.py
+++ news/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'pages',
+    "crispy_forms", # new
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "accounts.CustomUser"
 LOGIN_REDIRECT_URL = "home" 
 LOGOUT_REDIRECT_URL = "home" 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5" # new
+CRISPY_TEMPLATE_PACK = "bootstrap5"
